e9e3f0f797ba3ec202e3ae83159c385c95802051
Victor Golovin <rinoally@gmail.com>
2023-05-07 19:32:36 +1000

    Training JS variables

---
+++ 2. Variables.js
@@ -0,0 +1,25 @@
+// 1. Task
+
+const MY_NAME = 'Victor';
+
+// alert(MY_NAME);
+
+// ------------------------
+
+// 2.Task 
+
+const BANK_SCORE = "ID_12345";
+
+console.log(BANK_SCORE);
+
+// ------------------------
+
+// 3.Task
+
+const MY_AGE = 29;
+
+if (MY_AGE > 18) {
+    alert("Вам больше 18")
+} else {
+    alert("Вам меньше 18")
+}
