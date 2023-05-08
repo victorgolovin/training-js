0b4cdd00657700d6da6f731e71d43da932fe2be0
Victor Golovin <rinoally@gmail.com>
2023-05-08 23:36:42 +1000

    complite two task

--- 3. data_types.js
+++ 3. data_types.js
@@ -24,3 +24,46 @@ let dollar = 1;
 console.log(typeof dollar_to_ruble_exchange_rate);
 
 console.log(`${dollar} ${'Доллар США равно'} ${dollar_to_ruble_exchange_rate } ${'Российских рублей.'}`);
+
+// Задание 3
+// 1. Придумать и создать переменную, которая будет отражать факт добавления товара 
+// в список избранных товаров в интернет-магазине. Т.е. содержит ответ на вопрос "Товар в избранном?"
+// 2. Если товар в избранном, вывести в консоль текст "Товар в избранном"
+// 3. Если товар не в избранном, вывести в консоль текст "Товар не в избранном"
+
+const PRODUCT_IN_FAVORITES = true;
+const PRODUCT_NOT_IN_FAVORITES = false;
+let product_in_favorites_question = 'Товар в избранном?';
+
+console.log(product_in_favorites_question);
+
+if (PRODUCT_IN_FAVORITES > PRODUCT_NOT_IN_FAVORITES) {
+    console.log("Товар в избранном");
+} else {
+    console.log("Товар не в избранном");
+}
+
+// Задание 4
+// 1. Создать объект me
+// 2. Добавить в объект минимум 7 свойств, в которых будет раскрыта информация о вас. Например, имя, возраст и т.д.
+// 3. Вывести объект в консоль
+
+let age = 28;
+let sex = 'male';
+let first_name = 'Victor';
+let second_name = 'Golovin';
+let birth_date = '13.06.1994';
+let top_food = 'meat';
+let heith = 1.85;
+
+const me = {
+    Возраст: (age),
+    Пол: (sex),
+    Имя: (first_name),
+    Фамилия: (second_name),
+    Дата_Рождения: (birth_date),
+    Еда: (top_food),
+    Рост: (heith)
+}
+
+console.log(me);
